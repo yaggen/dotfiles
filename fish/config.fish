d231fb0463a0073351f52147841193b897377028
yaggen <jack@jackhack.me>
2025-11-01 12:50:30 +0100

    dotfiles fur alles

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+set -g glyph_status_nonzero "💀"
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -g fish_prompt_pwd_dir_length 0
+set -g color_dir_bg 89b4fa
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
